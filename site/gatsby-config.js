d10629ed3f8e1314634e59c8736f20ac429a3597
Balazs Karolyi <balazs.karolyi@hu.ibm.com>
2020-02-26 17:07:06 +0100

    feat(developer-site) initial gatsby setup

---
+++ site/gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Theme Carbon',
+    description: 'A Gatsby theme for the carbon design system',
+    keywords: 'gatsby,theme,carbon',
+  },
+  pathPrefix: `/gtc`,
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Carbon Design Gatsby Theme',
+        short_name: 'Gatsby Theme Carbon',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#0062ff',
+        display: 'browser',
+      },
+    },
+    {
+      resolve: 'gatsby-theme-carbon',
+      options: {
+        isSearchEnabled: true,
+        repository: {
+          baseUrl:
+            'https://github.com/carbon-design-system/gatsby-theme-carbon',
+          subDirectory: '/packages/example',
+        },
+      },
+    },
+  ],
+};
