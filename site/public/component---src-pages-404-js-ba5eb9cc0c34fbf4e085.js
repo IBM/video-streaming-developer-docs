f1f813b1820271934010a9262122f284acc42ded
Balazs Karolyi <balazs.karolyi@hu.ibm.com>
2020-04-16 20:27:51 +0200

    public folder initial

---
+++ site/public/component---src-pages-404-js-ba5eb9cc0c34fbf4e085.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n);t("q1tI");var o=t("Wjw8"),s=t("qKvR"),r=[{href:"/components/markdown",text:"Markdown"},{href:"/components/Aside",text:"Aside"},{href:"/components/demo",text:"Demo"}];n.default=function(){return Object(s.b)(o.a,{links:r})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ba5eb9cc0c34fbf4e085.js.map
