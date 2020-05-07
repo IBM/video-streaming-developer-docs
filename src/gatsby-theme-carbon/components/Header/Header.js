2b0edf442d983f09f0bce121753e60300e672df7
Balazs Karolyi <balazs.karolyi@hu.ibm.com>
2020-05-07 16:05:27 +0200

    feat(developer-site) fix header links

--- src/gatsby-theme-carbon/components/Header/Header.js
+++ src/gatsby-theme-carbon/components/Header/Header.js
@@ -1,17 +1,11 @@
 import React, { useContext } from 'react';
+import { Link } from 'gatsby';
 import {
-  Header as ShellHeader,
-  HeaderName,
-  HeaderNavigation,
-  HeaderMenuButton,
-  HeaderMenu,
-  HeaderMenuItem,
-  SkipToContent,
-  HeaderGlobalBar,
-  HeaderGlobalAction,
-  SideNav,
-  SideNavItems,
-  HeaderSideNavItems,
+    Header as ShellHeader,
+    HeaderMenuButton,
+    SkipToContent,
+    HeaderGlobalBar,
+    HeaderGlobalAction,
 } from 'carbon-components-react';
 import { AppSwitcher20, Close20 } from '@carbon/icons-react';
 import cx from 'classnames';
@@ -21,103 +15,118 @@ import NavContext from 'gatsby-theme-carbon/src/util/context/NavContext';
 import useMetadata from 'gatsby-theme-carbon/src/util/hooks/useMetadata';
 
 import {
-  header,
-  switcherButtonOpen,
-  skipToContent,
-  headerButton,
-  switcherButton,
+    header,
+    switcherButtonOpen,
+    skipToContent,
+    headerName,
+    collapsed,
+    headerButton,
+    switcherButton,
 } from 'gatsby-theme-carbon/src/components/Header/Header.module.scss';
 
+import {
+    headerLink,
+} from './HeaderSecondary.module.scss';
+
 const Header = ({ children }) => {
-  const {
-    leftNavIsOpen,
-    toggleNavState,
-    switcherIsOpen,
-  } = useContext(NavContext);
-  const { isSearchEnabled } = useMetadata();
+    const {
+        leftNavIsOpen,
+        toggleNavState,
+        switcherIsOpen,
+        searchIsOpen,
+    } = useContext(NavContext);
+    const { isSearchEnabled } = useMetadata();
 
-  const getMenuItems = () => (
-    <>
-      <HeaderMenuItem href="/api-basics-overview">
-        API basics
-      </HeaderMenuItem>
-      <HeaderMenuItem href="/channel-api-getting-started">
-        Channel API
-      </HeaderMenuItem>
-      <HeaderMenuItem href="/viewer-authentication-api-getting-started">
-        Viewer Authentication API
-      </HeaderMenuItem>
-      <HeaderMenuItem href="/player-api-getting-started">
-        Player API
-      </HeaderMenuItem>
-      <HeaderMenuItem href="/analytics-api-getting-started">
-        Analytics API
-      </HeaderMenuItem>
-      <HeaderMenu aria-label="Link 4" menuLinkName="Submenu">
-        <HeaderMenuItem href="#one">Sub-link 1</HeaderMenuItem>
-        <HeaderMenuItem href="#two">Sub-link 2</HeaderMenuItem>
-        <HeaderMenuItem href="#three">Sub-link 3</HeaderMenuItem>
-      </HeaderMenu>
-    </>
-  );
-
-  return (
-    <ShellHeader aria-label="Header" className={header}>
-      <SkipToContent href="#main-content" className={skipToContent} />
-      <HeaderMenuButton
-        className={cx('bx--header__action--menu', headerButton)}
-        aria-label="Open menu"
-        onClick={() => {
-          toggleNavState('leftNavIsOpen');
-          toggleNavState('switcherIsOpen', 'close');
-        }}
-        isActive={leftNavIsOpen}
-      />
-      <HeaderName href="/" prefix="IBM">
-        {children}
-      </HeaderName>
-      <HeaderNavigation aria-label={`IBM ${children}`}>
-        {getMenuItems()}
-      </HeaderNavigation>
-      <HeaderGlobalBar>
-        {isSearchEnabled && <GlobalSearch />}
-        <HeaderGlobalAction
-          className={cx(headerButton, switcherButton, {
-            [switcherButtonOpen]: switcherIsOpen,
-          })}
-          aria-label="Switch"
-          onClick={() => {
-            toggleNavState('switcherIsOpen');
-            toggleNavState('searchIsOpen', 'close');
-            toggleNavState('leftNavIsOpen', 'close');
-          }}
-        >
-          {switcherIsOpen ? <Close20 /> : <AppSwitcher20 />}
-        </HeaderGlobalAction>
-      </HeaderGlobalBar>
-      <SideNav
-        aria-label="Side navigation"
-        className="bx--side-nav--website--light"
-        expanded={leftNavIsOpen}
-        isPersistent={false}>
-        <SideNavItems>
-          <HeaderSideNavItems>
-            {getMenuItems()}
-          </HeaderSideNavItems>
-        </SideNavItems>
-      </SideNav>
-    </ShellHeader>
-  );
+    return (
+        <>
+            <ShellHeader aria-label="Header" className={header}>
+                <SkipToContent href="#main-content" className={skipToContent} />
+                <HeaderMenuButton
+                    className={cx('bx--header__action--menu', headerButton)}
+                    aria-label="Open menu"
+                    onClick={() => {
+                        toggleNavState('leftNavIsOpen');
+                        toggleNavState('switcherIsOpen', 'close');
+                    }}
+                    isActive={leftNavIsOpen}
+                />
+                <Link
+                    className={cx(headerName, {
+                        [collapsed]: searchIsOpen,
+                    }, headerLink)}
+                    to="/"
+                >
+                    {children}
+                </Link>
+                <Link
+                    className={cx(headerName, {
+                        [collapsed]: searchIsOpen,
+                    }, headerLink)}
+                    to="/api-basics-overview"
+                >
+                    API basics
+                </Link>
+                <Link
+                    className={cx(headerName, {
+                        [collapsed]: searchIsOpen,
+                    }, headerLink)}
+                    to="/channel-api-getting-started"
+                >
+                    Channel API
+                </Link>
+                <Link
+                    className={cx(headerName, {
+                        [collapsed]: searchIsOpen,
+                    }, headerLink)}
+                    to="/viewer-authentication-api-getting-started"
+                >
+                    Viewer Authentication API
+                </Link>
+                <Link
+                    className={cx(headerName, {
+                        [collapsed]: searchIsOpen,
+                    }, headerLink)}
+                    to="/player-api-getting-started"
+                >
+                    Player API
+                </Link>
+                <Link
+                    className={cx(headerName, {
+                        [collapsed]: searchIsOpen,
+                    }, headerLink)}
+                    to="/analytics-api-getting-started"
+                >
+                    Analytics API
+                </Link>
+                <HeaderGlobalBar>
+                    {isSearchEnabled && <GlobalSearch />}
+                    <HeaderGlobalAction
+                        className={cx(headerButton, switcherButton, {
+                            [switcherButtonOpen]: switcherIsOpen,
+                        })}
+                        aria-label="Switch"
+                        onClick={() => {
+                            toggleNavState('switcherIsOpen');
+                            toggleNavState('searchIsOpen', 'close');
+                            toggleNavState('leftNavIsOpen', 'close');
+                        }}
+                    >
+                        {switcherIsOpen ? <Close20 /> : <AppSwitcher20 />}
+                    </HeaderGlobalAction>
+                </HeaderGlobalBar>
+            </ShellHeader>
+        </>
+    );
 };
 
 const DefaultHeaderText = () => (
-  <>
-    Video Streaming
-  </>
+    <>
+        Carbon&nbsp;<span>Design System</span>
+    </>
 );
 
 Header.defaultProps = {
-  children: <DefaultHeaderText />,
+    children: <DefaultHeaderText />,
 };
 
 export default Header;
