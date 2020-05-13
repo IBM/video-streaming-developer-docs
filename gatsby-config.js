1b9c398f23ca0c41a696f9ac76d3bed90cbd4fee
Mariusz Mickiewicz <Mariusz.Mickiewicz@hu.ibm.com>
2020-05-13 18:19:35 +0200

    feat(meta): updated page title and other meta
    somehow description and keywords are not in the output code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Theme Carbon',
-    description: 'A Gatsby theme for the carbon design system',
-    keywords: 'gatsby,theme,carbon',
+    title: 'IBM Video Streaming Developers',
+    description: 'Customize and control the viewing experience for your audience or create your own streaming applications, analyze engagement and telemetry data with the APIs and SDKs of the IBM Video Streaming platform.',
+    keywords: 'gatsby,video,video streaming,developer,developers,sdk,api,player,ios,android,broadcast',
   },
   pathPrefix: `/ustream-web/developers`,
   plugins: [
