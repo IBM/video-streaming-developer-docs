cdcf3e2d444369c124f0b7de02524563f93c5241
Balint Lendvai <balint.lendvai@ibm.com>
2020-05-22 12:00:04 +0200

    fix(gatsby-config): auto-fix lint issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
     title: 'IBM Video Streaming Developers',
-    description: 'Customize and control the viewing experience for your audience or create your own streaming applications, analyze engagement and telemetry data with the APIs and SDKs of the IBM Video Streaming platform.',
+    description:
+      'Customize and control the viewing experience for your audience or create your own streaming applications, analyze engagement and telemetry data with the APIs and SDKs of the IBM Video Streaming platform.',
     keywords: 'gatsby,video,video streaming,developer,developers,sdk,api,player,ios,android,broadcast',
   },
   pathPrefix: `/ustream-web/developers`,
-  //local build
-  //pathPrefix: `/developers/public/`,
+  // local build
+  // pathPrefix: `/developers/public/`,
   plugins: [
     {
       resolve: 'gatsby-plugin-manifest',
@@ -24,8 +25,7 @@ module.exports = {
       options: {
         isSearchEnabled: true,
         repository: {
-          baseUrl:
-            'https://github.com/carbon-design-system/gatsby-theme-carbon',
+          baseUrl: 'https://github.com/carbon-design-system/gatsby-theme-carbon',
           subDirectory: '/packages/example',
         },
       },
