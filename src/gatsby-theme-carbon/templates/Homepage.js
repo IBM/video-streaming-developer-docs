33dbaba1c05ec1984ac11de3b227190902d07251
Balint Lendvai <balint.lendvai@ibm.com>
2020-05-26 17:53:14 +0200

    style(homepage-callout): updated the background color

--- src/gatsby-theme-carbon/templates/Homepage.js
+++ src/gatsby-theme-carbon/templates/Homepage.js
@@ -4,26 +4,24 @@ import HomepageTemplate from './HomepageTemplate';
 
 import Carbon from '../../images/hero@2x.png';
 
-
 const FirstLeftText = () => <p>Developer Tools</p>;
-const FirstRightText = () => <p>Customize and control the viewing experience for your audience or create your own streaming applications, analyze engagement and telemetry data with the APIs and SDKs of the IBM Video Streaming platform.</p>;
-
+const FirstRightText = () => (
+  <p>
+    Customize and control the viewing experience for your audience or create your own streaming applications, analyze
+    engagement and telemetry data with the APIs and SDKs of the IBM Video Streaming platform.
+  </p>
+);
 
 const customProps = {
-    Banner: <HomepageBanner renderText={() => false} image={Carbon} />,
-    FirstCallout: (
-        <HomepageCallout
-          backgroundColor="#030303"
-          color="white"
-          leftText={FirstLeftText}
-          rightText={FirstRightText}
-        />
-      ),
+  Banner: <HomepageBanner renderText={() => false} image={Carbon} />,
+  FirstCallout: (
+    <HomepageCallout backgroundColor="#161616" color="white" leftText={FirstLeftText} rightText={FirstRightText} />
+  ),
 };
 
 // spreading the original props gives us props.children (mdx content)
 function ShadowedHomepage(props) {
-    return <HomepageTemplate {...props} {...customProps} />;
+  return <HomepageTemplate {...props} {...customProps} />;
 }
 
 export default ShadowedHomepage;
