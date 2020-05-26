d42be349e6808233ac319775062fe4311a315370
Balint Lendvai <balint.lendvai@ibm.com>
2020-05-26 17:30:22 +0200

    feat(homepage): add custom card layout

--- src/components/CustomCard/CustomCard.js
+++ src/components/CustomCard/CustomCard.js
@@ -2,11 +2,20 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ArrowRight20 } from '@carbon/icons-react';
 
-import { customCard, customCardTitle, customCardContent, customCardInner } from './CustomCard.module.scss';
+import {
+  customCard,
+  customCardDark,
+  customCardTitle,
+  customCardContent,
+  customCardInner,
+} from './CustomCard.module.scss';
 
-const CustomCard = ({ href, title, children }) => (
+const CustomCard = ({ href, title, children, theme }) => (
   <div className={`${customCard}`}>
-    <a href={href} className={`bx--tile bx--tile--clickable ${customCardInner}`}>
+    <a
+      href={href}
+      className={`bx--tile bx--tile--clickable ${customCardInner} ${theme === 'dark' ? customCardDark : ''}`}
+    >
       <h4 className={customCardTitle}>{title}</h4>
       <div className={customCardContent}>{children}</div>
       <div>
