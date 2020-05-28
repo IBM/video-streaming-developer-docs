3e42aeea00c7f9e977b50df403e4bb5a03ea5e77
Balint Lendvai <balint.lendvai@ibm.com>
2020-05-28 12:43:53 +0200

    fix(lint-errors): codestyle and eslint error fixes

--- src/gatsby-theme-carbon/components/Footer.js
+++ src/gatsby-theme-carbon/components/Footer.js
@@ -4,17 +4,13 @@ import Footer from './Footer/Footer';
 const Content = ({ buildTime }) => (
   <>
     <p>
-      The <code>Content</code> component receives a <code>buildTime</code> prop
-      that to display your site's build time: {buildTime}
+      The <code>Content</code> component receives a <code>buildTime</code> prop that to display your site's build time:{' '}
+      {buildTime}
     </p>
     <p>
-      By importing the <strong>Footer</strong> component from
-      gatsby-theme-carbon, we can supply our own props.
+      By importing the <strong>Footer</strong> component from gatsby-theme-carbon, we can supply our own props.
     </p>
-    <p>
-      The default export from a shadowed component will replace that component
-      in the theme.
-    </p>
+    <p>The default export from a shadowed component will replace that component in the theme.</p>
     <p>
       <a href="https://www.gatsbyjs.org/docs/themes/api-reference/#component-shadowing">
         More about component shadowing
--- src/gatsby-theme-carbon/components/Header/LeftNavHeaderMenus.js
+++ src/gatsby-theme-carbon/components/Header/LeftNavHeaderMenus.js
@@ -1,50 +1,40 @@
-import React, {useContext} from 'react';
-import Content from "./Content";
-
-import {withPrefix} from 'gatsby-link';
-import {SideNavMenu, SideNavMenuItem} from "carbon-components-react";
-import {Link} from "gatsby";
-import styles from "gatsby-theme-carbon/src/components/LeftNav/LeftNav.module.scss";
-import NavContext from "gatsby-theme-carbon/src/util/context/NavContext";
+import React, { useContext } from 'react';
+import { Link } from 'gatsby';
+import NavContext from 'gatsby-theme-carbon/src/util/context/NavContext';
+import { SideNavMenu, SideNavMenuItem } from 'carbon-components-react';
+import styles from 'gatsby-theme-carbon/src/components/LeftNav/LeftNav.module.scss';
+import Content from './Content';
 
 const HeaderMenus = () => {
-    const {
-        toggleNavState,
-    } = useContext(NavContext);
+  const { toggleNavState } = useContext(NavContext);
 
-    return (
-        <>
-            {Content
-                .map((mainMenu, i) => (
-                    <SideNavMenu
-                        icon={<span>dummy icon</span>}
-                        isActive={false}
-                        defaultExpanded={false}
-                        title={mainMenu.title}
-                        key={i}
-                    >
-                        {mainMenu.pages.map((subMenu, i) => (
-                            <SideNavMenuItem
-                                to={subMenu.path}
-                                onClick={() => {
-                                    toggleNavState('leftNavIsOpen', 'close');
-                                }}
-                                element={Link}
-                                isActive={false}
-                                key={i}
-                            >
-                                <span
-                                    className={styles.linkText}
-                                >
-                                  {subMenu.title}
-                                </span>
-                            </SideNavMenuItem>
-                        ))}
-                    </SideNavMenu>
-                )
-            )}
-        </>
-    );
+  return (
+    <>
+      {Content.map((mainMenu, index) => (
+        <SideNavMenu
+          icon={<span>dummy icon</span>}
+          isActive={false}
+          defaultExpanded={false}
+          title={mainMenu.title}
+          key={index}
+        >
+          {mainMenu.pages.map((subMenu, key) => (
+            <SideNavMenuItem
+              to={subMenu.path}
+              onClick={() => {
+                toggleNavState('leftNavIsOpen', 'close');
+              }}
+              element={Link}
+              isActive={false}
+              key={key}
+            >
+              <span className={styles.linkText}>{subMenu.title}</span>
+            </SideNavMenuItem>
+          ))}
+        </SideNavMenu>
+      ))}
+    </>
+  );
 };
 
 export default HeaderMenus;
