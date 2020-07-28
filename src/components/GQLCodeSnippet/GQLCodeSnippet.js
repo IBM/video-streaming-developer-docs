cc9a8f638ca0c3b66e222b165cece0c4d426ef04
Péter Attila Vásárhelyi <Peter.Vasarhelyi@hu.ibm.com>
2020-07-28 13:55:54 +0200

    feat(gqlcodesnippet): add component
    * feat(gqlcodesnippet): add component
    
    * docs(multiplefiles): ecdn api update and fixes

---
+++ src/components/GQLCodeSnippet/GQLCodeSnippet.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { row, container, highlight } from 'gatsby-theme-carbon/src/components/Code/Code.module.scss';
+import { gqlCodeSnippet, dark, link, keyword, attribute, operator, comment, line } from './GQLCodeSnippet.module.scss';
+
+const GQLCodeSnippet = ({ children }) => (
+  <div className={`bx--row ${row} ${gqlCodeSnippet}`}>
+    <div className={container}>
+      <pre className={`${highlight} ${dark}`}>{children}</pre>
+    </div>
+  </div>
+);
+
+export default GQLCodeSnippet;
+
+export function GQLOpen() {
+  return <span>{'{'}</span>;
+}
+
+export function GQLClose() {
+  return <span>{'}'}</span>;
+}
+
+export function GQLLine({ children }) {
+  return <div className={line}>{children}</div>;
+}
+
+export function GQLTab() {
+  return <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>;
+}
+
+export function GQLName({ children }) {
+  return <span>{children}</span>;
+}
+
+export function GQLKeyword({ children }) {
+  return <span className={keyword}>{children}</span>;
+}
+
+export function GQLAttribute({ children }) {
+  return <span className={attribute}>{children}</span>;
+}
+
+export function GQLComment({ children }) {
+  return <span className={comment}>{children}</span>;
+}
+
+export function GQLOperator({ children }) {
+  return <span className={operator}>{children}</span>;
+}
+
+export function GQLLink({ children, href }) {
+  return (
+    <a href={href} className={link}>
+      {children}
+    </a>
+  );
+}
