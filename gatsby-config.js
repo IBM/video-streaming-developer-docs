e085c449959593acd70af4b297e1344dfcbda61b
Balint Lendvai <balint.lendvai@ibm.com>
2021-09-18 23:52:00 +0200

    fix(manifest): after gatsby upgrade icon is now necessary value

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
         background_color: '#ffffff',
         theme_color: '#0062ff',
         display: 'browser',
+        icon: 'src/images/favicon.svg',
       },
     },
     {
