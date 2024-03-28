0b530669c364261157fd4fce079ff50cf7d834b0
Balazs Karolyi <balazs.karolyi@hu.ibm.com>
2024-03-28 15:12:57 +0100

    feat(sdk): ivsc-34 Remove SDKs - update gatsby theme carbon

--- src/gatsby-theme-carbon/components/HeaderNav/HeaderNav.js
+++ src/gatsby-theme-carbon/components/HeaderNav/HeaderNav.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 import { HeaderNavigation } from '@carbon/react';
+import HeaderNavItem from 'gatsby-theme-carbon/src/components/HeaderNav/HeaderNavItem';
+
+import MainNavContentHeader from '../MainNavContentHeader';
 
 function HeaderNav() {
+  const navItems = MainNavContentHeader;
   const label = 'IBM Video Streaming Developers Site';
 
-  return <HeaderNavigation className="cds--g100" aria-label={label} />;
+  return (
+    <HeaderNavigation className="cds--g100" aria-label={label}>
+      {navItems.map(({ pages, title }) => (
+        <HeaderNavItem items={pages} category={title} key={title} />
+      ))}
+    </HeaderNavigation>
+  );
 }
 
 export default HeaderNav;
